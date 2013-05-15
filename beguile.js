1473e3df079f494a265ca013feed56194ea840db
cliff <dunderjeep@gmail.com>
2013-05-15 15:51:10 -0400

    added Lightning Strike to collection, some css

--- beguile.js
+++ beguile.js
@@ -2,14 +2,6 @@ cards = new Meteor.Collection("Cards");
 
 if (Meteor.isClient) {
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-
 	Template.cards.cards = function() {
 		return cards.find({}, {sort: {Name: 1}});
 	};	
